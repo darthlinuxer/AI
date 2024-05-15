9386ee3ceff9a2e86e5d0910c56982e9455d4a71
darthlinuxer <chaves.camilo@gmail.com>
2024-05-15 15:56:21 -0300

    Refactored to create Python modules

---
+++ Tutorials_Basic/Modules/Rag/src/__init__.py
@@ -0,0 +1,7 @@
+__all__ = [
+    "Rag",
+    "SuperSourceLoaderTransformer",
+]
+
+from .Rag import Rag
+from .SuperSourceLoaderTransformer import SuperSourceLoaderTransformer
---
+++ Tutorials_Medium/rag_server.py
@@ -0,0 +1,36 @@
+from fastapi import FastAPI
+import logging
+from fastapi import FastAPI
+from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
+from otel_config import setup_tracing
+from rag_routes import router as rag_routes
+
+app = FastAPI()
+setup_tracing()
+
+app.include_router(rag_routes)
+
+FastAPIInstrumentor.instrument_app(app)
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+@app.get("/")
+async def index() -> str:
+    message = "Server is online!"
+    logger.info(message)
+    return message
+
+import uvicorn
+
+def run_server():
+    try:
+        uvicorn.run(app, host="127.0.0.1", port=8000, reload=False)
+    except Exception as e:
+        print(f"Error starting server: {e}")
+    finally:
+        print("Server stopped")
+
+
+if __name__ == "__main__":
+    run_server()
