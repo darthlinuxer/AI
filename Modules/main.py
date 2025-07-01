95aead3daadeda5415a919dc9c33255db9b5f9de
DarthLinuxer <chaves.camilo@gmail.com>
2025-07-01 08:58:23 -0300

    ChromaRepository update

---
+++ Modules/main.py
@@ -0,0 +1,7 @@
+from ChromaRepository import *
+
+print(ChromaRepository.__name__)
+print(ChromaRepository.__dict__)
+print(dir(ChromaRepository))
+
+print("Main executed!")
--- Tutorials_Basic/Modules/main.py
+++
@@ -1,7 +0,0 @@
-from ChromaRepository import *
-
-print(ChromaRepository.__name__)
-print(ChromaRepository.__dict__)
-print(dir(ChromaRepository))
-
-print("Main executed!")
